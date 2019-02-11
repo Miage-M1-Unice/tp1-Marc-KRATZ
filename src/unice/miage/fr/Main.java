194809a12af0223a071f67a61d8e09892d8e2f57
MarcusGH <kratzmarc@gmail.com>
2019-02-11 12:00:01 +0100

    question 2 premier partie

--- src/unice/miage/fr/Main.java
+++ src/unice/miage/fr/Main.java
@@ -7,14 +7,29 @@ public class Main{
         File[] paths;
         File f;
         try{
-            f = new File("/");
-            paths = f.listFiles();
+            f = new File("/Users/marc/Documents/TP_JAVA/TP01");
+             /**paths = f.listFiles();
 
             for(File path:paths) {
                 System.out.println(path);
-            }
+            }**/
+            recurrenceListFile(f);
         } catch(Exception e){
             e.printStackTrace();
+        }
+    }
+
+    public static void recurrenceListFile(File file){
+        File f = new File(file.toString());
+        File[] paths;
+        File test;
+        paths = f.listFiles();
+        for(File path:paths) {
+            System.out.println(path);
+            test = new File(path.toString());
+            if(test.isDirectory()){
+                recurrenceListFile(path);
+            }
         }
     }
 }
